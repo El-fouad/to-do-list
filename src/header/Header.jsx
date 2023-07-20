ec8036752f7a6766fe0b6ae977e51d07f1da58a3
ELhamdaoui_Fouad <felhamd@gmail.com>
2023-07-20 19:40:51 +0100

    install react icons  & styled header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,25 +1,30 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addNewTask } from "../actions/actions";
+import "./Header.css";
 
 function Header() {
   const [newTask, setnewTask] = useState();
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const handleAddClick =()=>{
-    dispatch(addNewTask(newTask))
-  }
+  const handleAddClick = () => {
+    if (newTask) {
+      dispatch(addNewTask(newTask));
+      setnewTask("");
+    }
+  };
   return (
     <>
-      <h1>To Do List</h1>
+      <h1 className="title">To Do List</h1>
       <div className="formAddTask">
         <input
           type="text"
           placeholder="Add a new task "
           onChange={(e) => setnewTask(e.target.value)}
+          value={newTask}
         />
-        <button onClick={()=>handleAddClick()}>+</button>
+        <button onClick={() => handleAddClick()}>+</button>
       </div>
     </>
   );

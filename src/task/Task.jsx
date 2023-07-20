37967916c12c37044aa3c29d049c0d9ca77615f2
ELhamdaoui_Fouad <felhamd@gmail.com>
2023-07-20 11:25:44 +0100

    add container of task

---
+++ src/task/Task.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import "./Task.css";
+import { DataTasks } from "../data";
+
+function Task(props) {
+  const [isCheck, setisCheck] = useState(props.isChecked);
+  const [trashTask, setTrashTask] = useState();
+  useEffect(() => {
+    setisCheck(props.isChecked)
+
+    setTrashTask(isCheck ? { textDecoration: "line-through" } : null);
+    
+  },[isCheck]);
+  console.log('state lesss ',isCheck);
+  const handleChange = () => {
+    setisCheck(!isCheck);
+  };
+  return (
+    <>
+      <div className="taskBody" >
+        <h5 style={trashTask} >{props.nameTask} </h5>
+        <div className="actions">
+          <input
+            type="checkbox"
+            className="checkbox"
+            onChange={() => handleChange()}
+            onClick={props.onClick}
+            checked={isCheck}
+          />
+          <button>delete</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Task;

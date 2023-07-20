fb9130d41243d4052acf0b73089ce4bd2ae2fedc
ELhamdaoui_Fouad <felhamd@gmail.com>
2023-07-20 11:28:18 +0100

    add data

---
+++ src/data.js
@@ -0,0 +1,31 @@
+
+   const DataTasks= [
+      {
+        "id": 1,
+        "title": "Research competitors",
+        "status": true
+      },
+      {
+        "id": 2,
+        "title": "Design wireframes",
+        "status": false
+      },
+      {
+        "id": 3,
+        "title": "Write copy for homepage",
+        "status":true
+      },
+      {
+        "id": 4,
+        "title": "Develop landing page",
+        "status": false
+      },
+      {
+        "id": 5,
+        "title": "Test landing page",
+        "status": false
+      }
+    ]
+
+
+    export {DataTasks} 
